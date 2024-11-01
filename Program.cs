6f64d959e86e8918bcc9e80d99a3a9509acdd9d9
Douglas Soares <douglassoares.cinema@gmail.com>
2024-10-31 21:35:06 -0300

    Entrega do desafio

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nome = "";
+        string nivel = "";
+        float xp = 0;
+
+        Console.WriteLine("Bem-vindo ao Classificador de Herói! Vamos descobrir qual o seu nível, baseado no seu nível de XP! Primeiro, digite seu nome:");
+        Console.Write("\n");
+        nome = Console.ReadLine() ?? "Cavaleiro Sem Nome da Silva";
+
+        Console.WriteLine("Agora, digite seu nível de XP:\n");
+        xp = float.Parse(Console.ReadLine() ?? "0");
+        
+        Console.Write("\n");
+
+        if (xp <= 1000)
+        {
+            nivel = "FERRO";
+        }
+        else if (xp <= 2000)
+        {
+            nivel = "BRONZE";
+        }
+        else if (xp <= 5000)
+        {
+            nivel = "PRATA";
+        }
+        else if (xp <= 6000)
+        {
+            nivel = "OURO";
+        }
+        else if (xp <= 7000)
+        {
+            nivel = "PLATINA";
+        }
+        else if (xp <= 8000)
+        {
+            nivel = "DIAMANTE";
+        }
+        else if (xp <= 9000)
+        {
+            nivel = "ASCENDENTE";
+        }
+        else if (xp <= 1000)
+        {
+            nivel = "IMORTAL";
+        }
+        else if (xp > 10000)
+        {
+            nivel = "RADIANTE";
+        }
+        else{
+            nivel = "INVÁLIDO";
+        }
+
+        Console.WriteLine($@"
+        O Herói de nome {nome} está no nível {nivel} com {xp} XP.
+        ");
+    }
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+# Desafio de Classificador de nível Herói 
+## Desafio do Felipão da DIO no Bootcamp "GFT Start #6 - Lógica de Programação"
+&nbsp;
+
+**O que deve ser utilizado**
+- Variáveis
+- Operadores
+- Laços de repetição
+- Estruturas de decisões
+
+&nbsp;
+
+## Objetivo
+
+Crie uma variável para armazenar o nome e a quantidade de experiência (XP) de um herói, depois utilize uma estrutura de decisão para apresentar alguma das mensagens abaixo:
+
+- Se XP for menor ou igual a 1.000 = Ferro
+&nbsp;
+
+- Se XP for entre 1.001 e 2.000 = Bronze
+&nbsp;
+
+- Se XP for entre 2.001 e 5.000 = Prata
+&nbsp;
+
+- Se XP for entre 5.001 e 6.000 = Ouro
+&nbsp;
+
+- Se XP for entre 6.001 e 7.000 = Platina
+&nbsp;
+
+- Se XP for entre 7.001 e 8.000 = Diamante
+&nbsp;
+
+- Se XP for entre 8.001 e 9.000 = Ascendente
+&nbsp;
+
+- Se XP for entre 9.001 = 10.000 = Imortal
+&nbsp;
+
+- Se XP for igual ou maior que 10.001 = Radiante
+&nbsp;
+
+## Saída
+Ao final deve-se exibir uma mensagem:
+- "O Herói de nome **{nome}** está no nível de **{nivel}**
